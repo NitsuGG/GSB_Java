675db97654653ad032132c489d73da1089002c18
Adrien <croizer.adrien@outlook.fr>
2021-10-14 14:19:16 +0200

    connexion et mysql connector(?)

--- src/gsb/modele/dao/ConnexionMySql.java
+++ src/gsb/modele/dao/ConnexionMySql.java
@@ -33,7 +33,7 @@ public class ConnexionMySql { // DAO = Data Access Object
 	 */
 	public static void connecterBd(){
 		//connexion à la base de donnée à partir de jdbc
-		String url = "jdbc:mysql://192.168.60.62:3306/gsb"; // url : chaine de connexion
+		String url = "jdbc:mysql://192.177.1.13:3306/GSB"; // url : chaine de connexion
 		// try permet d'essayer de lancer la connexion
 		try {Class.forName("com.mysql.jdbc.Driver"); 
 			cnx = DriverManager.getConnection(url,"usergsb","password"); 
