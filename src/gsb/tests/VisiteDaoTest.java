731881c9dc75b9f83a631fb6626bfae3b75a58a4
Adrien <croizer.adrien@outlook.fr>
2021-11-18 15:51:25 +0100

    visiteDaoTest

--- src/gsb/tests/VisiteDaoTest.java
+++ src/gsb/tests/VisiteDaoTest.java
@@ -1,9 +1,17 @@
 package gsb.tests;
 
 import gsb.modele.Visite;
+import gsb.modele.dao.VisiteDao;
+import gsb.utils.*;
+
 public class VisiteDaoTest {
 	
-	public static void man(String[] args) {
+	public static void main(String[] args) {
+		
+		Visite laVisite = VisiteDao.rechercher("v0004");
+		
+		AffichageModele.afficherVisite(laVisite);
+		
 		
 	}
 }
